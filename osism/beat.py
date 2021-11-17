008943af5a084807e185bd2d2224bfde1b4604cc
Christian Berendt <berendt@osism.tech>
2021-11-17 16:31:23 +0100

    Gather facts every 600 seconds
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/beat.py
+++ osism/beat.py
@@ -9,5 +9,5 @@ class Run(Command):
     log = logging.getLogger(__name__)
 
     def take_action(self, parsed_args):
-        p = subprocess.Popen(f"celery --broker=redis://redis beat -s /tmp/celerybeat-schedule.db", shell=True)
+        p = subprocess.Popen(f"celery -A osism.tasks.ansible --broker=redis://redis beat -s /tmp/celerybeat-schedule.db", shell=True)
         p.wait()
--- osism/tasks/ansible.py
+++ osism/tasks/ansible.py
@@ -6,6 +6,16 @@ from osism.tasks import Config, run_ansible_in_environment
 app = Celery('ansible')
 app.config_from_object(Config)
 
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(600.0, gather_facts.s(), expires=10)
+
+
+@app.task(bind=True, name="osism.tasks.ansible.run")
+def gather_facts(self):
+    run_ansible_in_environment(self.request.id, "generic", "facts", [])
+
+
 @app.task(bind=True, name="osism.tasks.ansible.run")
 def run(self, environment, playbook, arguments):
     run_ansible_in_environment(self.request.id, environment, playbook, arguments)
