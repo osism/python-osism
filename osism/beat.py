1373fa00d9d0c11fddc99c199cd66dc17bc6a3b5
Christian Berendt <berendt@osism.tech>
2021-11-17 16:18:08 +0100

    Store the beat schedule db in /tmp
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/beat.py
+++ osism/beat.py
@@ -9,5 +9,5 @@ class Run(Command):
     log = logging.getLogger(__name__)
 
     def take_action(self, parsed_args):
-        p = subprocess.Popen(f"celery --broker=redis://redis beat", shell=True)
+        p = subprocess.Popen(f"celery --broker=redis://redis beat -s /tmp/celerybeat-schedule.db", shell=True)
         p.wait()
