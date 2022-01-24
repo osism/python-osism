c8af4be19f33703884e3a4b88a8ed085967e4ec3
Christian Berendt <berendt@osism.tech>
2022-01-24 19:40:01 +0100

    api: add /webhook/netbox endpoint
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/api.py
+++ osism/api.py
@@ -1,5 +1,24 @@
-from fastapi import FastAPI
+import datetime
+from uuid import UUID
+
+from fastapi import FastAPI, Header, Request, Response
+from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
+
+
+class WebhookNetboxResponse(BaseModel):
+    result: str
+
+
+class WebhookNetboxData(BaseModel):
+    username: str
+    data: dict
+    snapshots: dict
+    event: str
+    timestamp: datetime.datetime
+    model: str
+    request_id: UUID
+
 
 app = FastAPI()
 
@@ -11,3 +30,14 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/webhook/netbox", response_model=WebhookNetboxResponse, status_code=200)
+async def webhook(
+    webhook_input: WebhookNetboxData,
+    request: Request,
+    response: Response,
+    content_length: int = Header(...),
+    x_hook_signature: str = Header(None)
+):
+    return {"result": "ok"}
