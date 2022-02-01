6a6e4db7f8fb86290a8b6e5cb1d3f1c873fa35eb
Christian Berendt <berendt@osism.tech>
2022-02-01 20:16:37 +0100

    Move first method to osism.utils
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ osism/utils/__init__.py
@@ -0,0 +1,22 @@
+# https://stackoverflow.com/questions/2361426/get-the-first-item-from-an-iterable-that-matches-a-condition
+def first(iterable, condition=lambda x: True):
+    """
+    Returns the first item in the `iterable` that
+    satisfies the `condition`.
+
+    If the condition is not given, returns the first item of
+    the iterable.
+
+    Raises `StopIteration` if no item satysfing the condition is found.
+
+    >>> first( (1,2,3), condition=lambda x: x % 2 == 0)
+    2
+    >>> first(range(3, 100))
+    3
+    >>> first( () )
+    Traceback (most recent call last):
+    ...
+    StopIteration
+    """
+
+    return next(x for x in iterable if condition(x))
