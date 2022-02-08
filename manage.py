a17b43c49f7eb85a87ca3a0f571f2c1b8be42339
Christian Berendt <berendt@osism.tech>
2022-02-08 19:19:16 +0100

    Remove unused manage.py script
    Closes #14
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- manage.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from oslo_config import cfg
-from oslo_service import service
-
-if __name__ == "__main__":
-    CONF = cfg.CONF
-    launcher = service.launch(CONF, service, workers=2)
