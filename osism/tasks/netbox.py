9ca740729059deae3a3d3ddc4d9f5643d8ab4d0a
Christian Berendt <berendt@osism.tech>
2022-04-24 20:32:15 +0200

    netbox: fix wrong sync methods
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/tasks/netbox.py
+++ osism/tasks/netbox.py
@@ -43,15 +43,15 @@ def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(300.0, periodic_synchronize_bifrost.s(), expires=10)
 
 
-@app.task(bind=True, name="osism.tasks.netbox.periodic_synchronize_bifrost")
-def periodic_synchronize_bifrost(self):
-    """Synchronize the state of Bifrost with Netbox"""
-    openstack.baremetal_node_list.apply_async((), link=synchronize_device_state.s())
-
-
 @app.task(bind=True, name="osism.tasks.netbox.periodic_synchronize_ironic")
 def periodic_synchronize_ironic(self):
     """Synchronize the state of Ironic with Netbox"""
+    openstack.baremetal_node_list.apply_async((), link=synchronize_device_state.s())
+
+
+@app.task(bind=True, name="osism.tasks.netbox.periodic_synchronize_bifrost")
+def periodic_synchronize_bifrost(self):
+    """Synchronize the state of Bifrost with Netbox"""
     ansible.run.apply_async(("manager", "bifrost-command", "baremetal node list -f json"), link=synchronize_device_state.s())
 
 
