752edc3da37b783a7bb9a3d3f11ccab68ebf1153
Christian Berendt <berendt@osism.tech>
2022-06-15 13:51:35 +0200

    Improve the compose command
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/compose.py
+++ osism/commands/compose.py
@@ -11,11 +11,20 @@ class Run(Command):
 
     def get_parser(self, prog_name):
         parser = super(Run, self).get_parser(prog_name)
+        parser.add_argument('target', nargs=1, type=str, help='Hostname or address')
+        parser.add_argument('environment', nargs=1, type=str, help='Environment')
         parser.add_argument('arguments', nargs=argparse.REMAINDER, help='Other arguments for Docker compose')
         return parser
 
     def take_action(self, parsed_args):
+        target_hostname = parsed_args.target[0]
+        environment = parsed_args.environment[0]
         arguments = parsed_args.arguments
-        docker_compose_command = "/usr/bin/docker compose"
 
-        subprocess.call(f"{docker_compose_command} {arguments}", shell=True)
+        ssh_command = f"docker compose --project-directory=/opt/{environment} {arguments}"
+        ssh_options = "-o StrictHostKeyChecking=no"
+
+        subprocess.call(f"{ssh_command} {arguments}", shell=True)
+
+        # FIXME: use paramiko or something else more Pythonic + make operator user + key configurable
+        subprocess.call(f"/usr/bin/ssh -i /ansible/secrets/id_rsa.operator {ssh_options} dragon@{target_hostname} {ssh_command}", shell=True)
