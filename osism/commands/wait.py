76babd7c8c752759ed5d2fe19a2d0e6e4f7664ce
Christian Berendt <berendt@osism.tech>
2022-09-04 19:57:05 +0200

    If task is already finished, get result from the backend
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/wait.py
+++ osism/commands/wait.py
@@ -1,7 +1,11 @@
 import logging
 
+from celery import Celery
+from celery.result import AsyncResult
 from cliff.command import Command
 from redis import Redis
+
+from osism.tasks import Config
 
 redis = Redis(host="redis", port="6379")
 
@@ -18,18 +22,26 @@ class Run(Command):
     def take_action(self, parsed_args):
         task_id = parsed_args.task_id[0]
 
-        p = redis.pubsub()
-        p.subscribe(f"{task_id}")
+        app = Celery('wait')
+        app.config_from_object(Config)
 
-        while True:
-            for m in p.listen():
-                if type(m["data"]) == bytes:
-                    line = m["data"].decode("utf-8")
-                    if line.startswith("RC: "):
-                        rc = int(line[4:])
-                        continue
-                    if line == "QUIT":
-                        redis.close()
-                        # NOTE: Use better solution
-                        return rc
-                    print(line, end="")
+        result = AsyncResult(f"{task_id}", app=app)
+
+        if result.state == 'SUCCESS':
+            print(result.get())
+        else:
+            p = redis.pubsub()
+            p.subscribe(f"{task_id}")
+
+            while True:
+                for m in p.listen():
+                    if type(m["data"]) == bytes:
+                        line = m["data"].decode("utf-8")
+                        if line.startswith("RC: "):
+                            rc = int(line[4:])
+                            continue
+                        if line == "QUIT":
+                            redis.close()
+                            # NOTE: Use better solution
+                            return rc
+                        print(line, end="")
