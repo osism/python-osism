6be6c15aff5ad2e910a6490f613c53e9e308a417
Christian Berendt <berendt@osism.tech>
2022-09-04 22:12:22 +0200

    Improve the output of the wait command for use in scripts (#176)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/wait.py
+++ osism/commands/wait.py
@@ -17,12 +17,14 @@ class Run(Command):
         parser = super(Run, self).get_parser(prog_name)
         parser.add_argument('--delay', default=1, type=int, help='Delay in seconds between two task checks')
         parser.add_argument('--live', default=False, help='Show live output from a started task until it is finished', action='store_true')
+        parser.add_argument('--format', default="log", help='Output type', const='log', nargs='?', choices=['script', 'log']),
         parser.add_argument('--output', default=False, help='Show output from a finished task', action='store_true')
         parser.add_argument('task_id', nargs='+', type=str, help='ID of tasks to wait for')
         return parser
 
     def take_action(self, parsed_args):
         delay = parsed_args.delay
+        format = parsed_args.format
         live = parsed_args.live
         output = parsed_args.output
         task_ids = parsed_args.task_id
@@ -37,17 +39,30 @@ class Run(Command):
             result = AsyncResult(f"{task_id}", app=app)
 
             if result.state == 'PENDING':
-                self.log.info(f"Task {task_id} is in state PENDING")
+
+                if format == "log":
+                    self.log.info(f"Task {task_id} is in state PENDING")
+                elif format == "script":
+                    self.log.info(f"{task_id} = PENDING")
+
                 task_ids.insert(0, task_id)
 
             elif result.state == 'SUCCESS':
-                self.log.info(f"Task {task_id} is in state SUCCESS")
+
+                if format == "log":
+                    self.log.info(f"Task {task_id} is in state SUCCESS")
+                elif format == "script":
+                    self.log.info(f"{task_id} = SUCCESS")
 
                 if output:
                     print(result.get())
 
             elif result.state == 'STARTED':
-                self.log.info(f"Task {task_id} is in state STARTED")
+
+                if format == "log":
+                    self.log.info(f"Task {task_id} is in state STARTED")
+                elif format == "script":
+                    self.log.info(f"{task_id} = STARTED")
 
                 if live:
                     redis = Redis(host="redis", port="6379")
