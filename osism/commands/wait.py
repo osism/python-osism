2eed9537208c8f8135cf7b0fca825d010b7460b0
Christian Berendt <berendt@osism.tech>
2022-09-04 21:39:56 +0200

    Improve waiting for tasks
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/wait.py
+++ osism/commands/wait.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 from celery import Celery
 from celery.result import AsyncResult
@@ -14,12 +15,14 @@ class Run(Command):
 
     def get_parser(self, prog_name):
         parser = super(Run, self).get_parser(prog_name)
+        parser.add_argument('--delay', default=1, type=int, help='Delay in seconds between two task checks')
         parser.add_argument('--live', default=False, help='Show live output from a started task until it is finished', action='store_true')
         parser.add_argument('--output', default=False, help='Show output from a finished task', action='store_true')
         parser.add_argument('task_id', nargs='+', type=str, help='ID of tasks to wait for')
         return parser
 
     def take_action(self, parsed_args):
+        delay = parsed_args.delay
         live = parsed_args.live
         output = parsed_args.output
         task_ids = parsed_args.task_id
@@ -27,32 +30,46 @@ class Run(Command):
         app = Celery('wait')
         app.config_from_object(Config)
 
-        for task_id in task_ids:
+        while task_ids:
+            time.sleep(delay)
+
+            task_id = task_ids.pop()
             result = AsyncResult(f"{task_id}", app=app)
 
-            if result.state == 'SUCCESS':
+            if result.state == 'PENDING':
+                self.log.info(f"Task {task_id} is in state PENDING")
+                task_ids.insert(0, task_id)
+
+            elif result.state == 'SUCCESS':
+                self.log.info(f"Task {task_id} is in state SUCCESS")
+
                 if output:
                     print(result.get())
 
-            elif result.state == 'STARTED' and live:
-                redis = Redis(host="redis", port="6379")
-                p = redis.pubsub()
-                p.subscribe(f"{task_id}")
+            elif result.state == 'STARTED':
+                self.log.info(f"Task {task_id} is in state STARTED")
 
-                while_True = True
-                while while_True:
-                    for m in p.listen():
-                        if type(m["data"]) == bytes:
-                            line = m["data"].decode("utf-8")
-                            if line.startswith("RC: "):
-                                rc = int(line[4:])
-                                continue
-                            elif line == "QUIT":
-                                redis.close()
+                if live:
+                    redis = Redis(host="redis", port="6379")
+                    p = redis.pubsub()
+                    p.subscribe(f"{task_id}")
 
-                                if len(task_ids) == 1:
-                                    return rc
+                    while_True = True
+                    while while_True:
+                        for m in p.listen():
+                            if type(m["data"]) == bytes:
+                                line = m["data"].decode("utf-8")
+                                if line.startswith("RC: "):
+                                    rc = int(line[4:])
+                                    continue
+                                elif line == "QUIT":
+                                    redis.close()
+
+                                    if len(task_ids) == 1:
+                                        return rc
+                                    else:
+                                        while_True = False
                                 else:
-                                    while_True = False
-                            else:
-                                print(line, end="")
+                                    print(line, end="")
+                else:
+                    task_ids.insert(0, task_id)
