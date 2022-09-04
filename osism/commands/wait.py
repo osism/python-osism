d6f36bcf93ae89ed1a0d81a887604e58780768dc
Christian Berendt <berendt@osism.tech>
2022-09-04 20:49:29 +0200

    Make it possible to wait for multiple tasks
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/wait.py
+++ osism/commands/wait.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 
 from celery import Celery
@@ -7,8 +8,6 @@ from redis import Redis
 
 from osism.tasks import Config
 
-redis = Redis(host="redis", port="6379")
-
 
 class Run(Command):
 
@@ -16,32 +15,45 @@ class Run(Command):
 
     def get_parser(self, prog_name):
         parser = super(Run, self).get_parser(prog_name)
-        parser.add_argument('task_id', nargs=1, type=str, help='Task ID')
+        parser.add_argument('--live', default=False, help='Show live output from a started task until it is finished', action='store_true')
+        parser.add_argument('--output', default=False, help='Show output from a finished task', action='store_true')
+        parser.add_argument('task_id', nargs=argparse.REMAINDER, type=str, help='ID of tasks to wait for')
         return parser
 
     def take_action(self, parsed_args):
-        task_id = parsed_args.task_id[0]
+        live = parsed_args.live
+        output = parsed_args.output
+        task_ids = parsed_args.task_id
 
         app = Celery('wait')
         app.config_from_object(Config)
 
-        result = AsyncResult(f"{task_id}", app=app)
+        for task_id in task_ids:
+            result = AsyncResult(f"{task_id}", app=app)
 
-        if result.state == 'SUCCESS':
-            print(result.get())
-        else:
-            p = redis.pubsub()
-            p.subscribe(f"{task_id}")
+            if result.state == 'SUCCESS':
+                if output:
+                    print(result.get())
 
-            while True:
-                for m in p.listen():
-                    if type(m["data"]) == bytes:
-                        line = m["data"].decode("utf-8")
-                        if line.startswith("RC: "):
-                            rc = int(line[4:])
-                            continue
-                        if line == "QUIT":
-                            redis.close()
-                            # NOTE: Use better solution
-                            return rc
-                        print(line, end="")
+            elif result.state == 'STARTED' and live:
+                redis = Redis(host="redis", port="6379")
+                p = redis.pubsub()
+                p.subscribe(f"{task_id}")
+
+                while_True = True
+                while while_True:
+                    for m in p.listen():
+                        if type(m["data"]) == bytes:
+                            line = m["data"].decode("utf-8")
+                            if line.startswith("RC: "):
+                                rc = int(line[4:])
+                                continue
+                            elif line == "QUIT":
+                                redis.close()
+
+                                if len(task_ids) == 1:
+                                    return rc
+                                else:
+                                    while_True = False
+                            else:
+                                print(line, end="")
