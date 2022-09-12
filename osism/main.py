4e71d03e440d03a8b51035d3d0f1ed47ef1a0fcd
Christian Berendt <berendt@osism.tech>
2022-09-12 08:58:25 +0200

    Bump version to 0.5.0 (#186)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/main.py
+++ osism/main.py
@@ -9,7 +9,7 @@ class OsismApp(App):
     def __init__(self):
         super(OsismApp, self).__init__(
             description='OSISM manager interface',
-            version='0.4.0',
+            version='0.5.0',
             command_manager=CommandManager('osism.commands'),
             deferred_help=True,
         )
