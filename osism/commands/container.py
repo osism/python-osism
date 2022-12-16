f08cf1df7969636c6971b7db74b3125156fd218c
Christian Berendt <berendt@osism.tech>
2022-12-16 21:33:36 +0100

    Add command argument to the container command
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/container.py
+++ osism/commands/container.py
@@ -1,3 +1,4 @@
+import argparse
 import subprocess
 
 from cliff.command import Command
@@ -7,13 +8,20 @@ class Run(Command):
     def get_parser(self, prog_name):
         parser = super(Run, self).get_parser(prog_name)
         parser.add_argument("target", nargs=1, type=str, help="Hostname or address")
+        parser.add_argument(
+            "command",
+            nargs=argparse.REMAINDER,
+            type=str,
+            help="Command to run",
+        )
         return parser
 
     def take_action(self, parsed_args):
         target_hostname = parsed_args.target[0]
+        command = " ".join(parsed_args.command)
 
-        ssh_command = "docker ps"
-        ssh_options = "-o StrictHostKeyChecking=no"
+        ssh_command = f"docker {command}"
+        ssh_options = "-o StrictHostKeyChecking=no -o LogLevel=ERROR"
 
         # FIXME: use paramiko or something else more Pythonic + make operator user + key configurable
         subprocess.call(
