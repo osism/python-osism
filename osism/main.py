a65442e57c821624e96ba8f48f32b2b11d784d02
Christian Berendt <berendt@osism.tech>
2022-12-16 19:53:05 +0100

    Do not output a hardcoded version (#247)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/main.py
+++ osism/main.py
@@ -3,12 +3,14 @@ import sys
 from cliff.app import App
 from cliff.commandmanager import CommandManager
 
+from . import __version__
+
 
 class OsismApp(App):
     def __init__(self):
         super(OsismApp, self).__init__(
             description="OSISM manager interface",
-            version="0.6.0",
+            version=__version__,
             command_manager=CommandManager("osism.commands"),
             deferred_help=True,
         )
