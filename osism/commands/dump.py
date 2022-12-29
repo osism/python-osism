c0b2ca553bdf02b4e8ee5f9942423afdfd29f89e
Christian Berendt <berendt@osism.tech>
2022-12-29 22:18:13 +0100

    Add initial dump facts command (#276)
    Part of osism/python-osism#274
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ osism/commands/dump.py
@@ -0,0 +1,68 @@
+# import argparse
+import pprint
+
+from cliff.command import Command
+import json
+from loguru import logger
+from tabulate import tabulate
+
+from osism.utils import redis
+
+
+class Facts(Command):
+    def get_parser(self, prog_name):
+        parser = super(Facts, self).get_parser(prog_name)
+        parser.add_argument(
+            "host",
+            nargs=1,
+            type=str,
+            help="Hostname (as the host is known in Ansible inventory)",
+        )
+        parser.add_argument(
+            "fact",
+            nargs="?",
+            type=str,
+            help="Name of a fact to show",
+        )
+        parser.add_argument(
+            "--no-cache",
+            default=False,
+            help="Do not use facts from the cache",
+            action="store_true",
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        host = parsed_args.host[0]
+        fact = parsed_args.fact
+        cache = not parsed_args.no_cache
+
+        data = redis.get(f"ansible_facts{host}")
+        if data:
+            data = json.loads(data)
+            table = []
+
+            if fact:
+                if fact in data:
+                    row = pprint.pformat(data[fact], indent=2, width=60, compact=True)
+                    table.append([host, fact, row])
+                else:
+                    logger.error(f"Fact {fact} not found in cache for {host}.")
+            else:
+                for fact in data:
+                    row = pprint.pformat(data[fact], indent=2, width=60, compact=True)
+                    if fact in [
+                        "ansible_ssh_host_key_dsa_public",
+                        "ansible_ssh_host_key_ecdsa_public",
+                        "ansible_ssh_host_key_ed25519_public",
+                        "ansible_ssh_host_key_rsa_public",
+                    ]:
+                        row = f"{row[0:40]}..."
+                    table.append([host, fact, row])
+
+            if table:
+                print(tabulate(table, headers=["Host", "Fact", "Value"], tablefmt="grid"))
+        else:
+            logger.error(f"No facts found in cache for {host}.")
+
+        return
