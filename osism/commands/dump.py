93262600ac11e78db3fbc63f44db989ff840c439
Christian Berendt <berendt@osism.tech>
2022-12-30 11:42:00 +0100

    Add initial dump inventory command
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/dump.py
+++ osism/commands/dump.py
@@ -1,5 +1,5 @@
-# import argparse
 import pprint
+import subprocess
 
 from cliff.command import Command
 import json
@@ -61,8 +61,63 @@ class Facts(Command):
                     table.append([host, fact, row])
 
             if table:
-                print(tabulate(table, headers=["Host", "Fact", "Value"], tablefmt="grid"))
+                print(
+                    tabulate(table, headers=["Host", "Fact", "Value"], tablefmt="grid")
+                )
         else:
             logger.error(f"No facts found in cache for {host}.")
+
+        return
+
+
+class Inventory(Command):
+    def get_parser(self, prog_name):
+        parser = super(Inventory, self).get_parser(prog_name)
+        parser.add_argument(
+            "host",
+            nargs=1,
+            type=str,
+            help="Hostname (as the host is known in Ansible inventory)",
+        )
+        parser.add_argument(
+            "variable",
+            nargs="?",
+            type=str,
+            help="Name of a variable to show",
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        host = parsed_args.host[0]
+        variable = parsed_args.variable
+
+        try:
+            result = subprocess.check_output(
+                f"ansible-inventory -i /ansible/inventory/hosts.yml --host {host}",
+                shell=True,
+                stderr=subprocess.DEVNULL,
+            )
+        except subprocess.CalledProcessError:
+            logger.error(f"Host {host} not found in inventory.")
+            return
+
+        data = json.loads(result)
+        table = []
+
+        if variable:
+            if variable in data:
+                row = pprint.pformat(data[variable], indent=2, width=60, compact=True)
+                table.append([host, variable, row])
+            else:
+                logger.error(f"Variable {variable} not found in inventory for {host}.")
+        else:
+            for variable in data:
+                row = pprint.pformat(data[variable], indent=2, width=60, compact=True)
+                table.append([host, variable, row])
+
+        if table:
+            print(
+                tabulate(table, headers=["Host", "Variable", "Value"], tablefmt="grid")
+            )
 
         return
