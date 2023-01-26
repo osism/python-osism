8c980238139ade76d9d0405402e886f51fb801c5
Christian Berendt <berendt@osism.tech>
2023-01-26 12:05:53 +0100

    Use /usr/local/bin/clush instead of /usr/bin/clush
    We now install Clush directly via Pypi and no longer via the
    package manager.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/console.py
+++ osism/commands/console.py
@@ -49,7 +49,7 @@ class Run(Command):
             subprocess.call(f"/run-ansible-console.sh {host}", shell=True)
         elif type_console == "clush":
             subprocess.call(
-                f"/usr/bin/clush -l dragon -w {host}",
+                f"/usr/local/bin/clush -l dragon -w {host}",
                 shell=True,
             )
         elif type_console == "ssh":
