2f9bfdf22fda156b15b9ef41a02d794b57cf7950
Christian Berendt <berendt@osism.tech>
2023-02-19 13:09:32 +0100

    Add refstack validator (#360)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/core/enums.py
+++ osism/core/enums.py
@@ -122,6 +122,7 @@ VALIDATE_PLAYBOOKS = {
     "container-status": {"environment": "generic", "runtime": "osism-ansible"},
     "kernel-version": {"environment": "generic", "runtime": "osism-ansible"},
     "mysql-open-files-limit": {"environment": "generic", "runtime": "osism-ansible"},
+    "refstack": {"environment": "openstack", "runtime": "osism-ansible"},
     "system-encoding": {"environment": "generic", "runtime": "osism-ansible"},
     "ulimits": {"environment": "generic", "runtime": "osism-ansible"},
 }
