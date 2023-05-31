4d9417c8baed8293ea302d82d216f326d74ea1cf
Christian Berendt <berendt@osism.tech>
2023-05-31 21:19:15 +0200

    Add vault password unset command
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/vault.py
+++ osism/commands/vault.py
@@ -4,9 +4,9 @@ from prompt_toolkit import prompt
 from osism.utils import redis
 
 
-class Password(Command):
+class SetPassword(Command):
     def get_parser(self, prog_name):
-        parser = super(Password, self).get_parser(prog_name)
+        parser = super(SetPassword, self).get_parser(prog_name)
         return parser
 
     def take_action(self, parsed_args):
@@ -14,3 +14,14 @@ class Password(Command):
         #       It's not ready in that form yet.
         ansible_vault_password = prompt("Ansible Vault password: ", is_password=True)
         redis.set("ansible_vault_password", ansible_vault_password)
+
+
+class UnsetPassword(Command):
+    def get_parser(self, prog_name):
+        parser = super(UnsetPassword, self).get_parser(prog_name)
+        return parser
+
+    def take_action(self, parsed_args):
+        # NOTE: This is a first step to make Ansible Vault usable via OSISM workers.
+        #       It's not ready in that form yet.
+        redis.delete("ansible_vault_password")
