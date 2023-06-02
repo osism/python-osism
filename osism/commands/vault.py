8d8e8fbfe625dacc49743e7947da3741fbfdae75
Christian Berendt <berendt@osism.tech>
2023-06-02 12:49:31 +0200

    Add cryptography to vault command
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/vault.py
+++ osism/commands/vault.py
@@ -1,19 +1,38 @@
+# NOTE: This is a first step to make Ansible Vault usable via OSISM workers.
+#       It's not ready in that form yet.
+
+import os
+
 from cliff.command import Command
+from cryptography.fernet import Fernet
 from prompt_toolkit import prompt
 
 from osism.utils import redis
 
 
 class SetPassword(Command):
+
+    keyfile = "/share/ansible_vault_password.key"
+
     def get_parser(self, prog_name):
         parser = super(SetPassword, self).get_parser(prog_name)
         return parser
 
     def take_action(self, parsed_args):
-        # NOTE: This is a first step to make Ansible Vault usable via OSISM workers.
-        #       It's not ready in that form yet.
+        if os.path.isfile(self.keyfile):
+            with open(self.keyfile, "r") as fp:
+                key = fp.read()
+        else:
+            key = Fernet.generate_key()
+            with open(self.keyfile, "w+") as fp:
+                fp.write(key.decode("utf-8"))
+
+        f = Fernet(key)
+
         ansible_vault_password = prompt("Ansible Vault password: ", is_password=True)
-        redis.set("ansible_vault_password", ansible_vault_password)
+        redis.set(
+            "ansible_vault_password", f.encrypt(ansible_vault_password.encode("utf-8"))
+        )
 
 
 class UnsetPassword(Command):
@@ -22,6 +41,4 @@ class UnsetPassword(Command):
         return parser
 
     def take_action(self, parsed_args):
-        # NOTE: This is a first step to make Ansible Vault usable via OSISM workers.
-        #       It's not ready in that form yet.
         redis.delete("ansible_vault_password")
