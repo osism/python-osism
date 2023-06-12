4c73a4936a563c4c9a8dc85a9d90b2c0a8bbb463
Christian Berendt <berendt@osism.tech>
2023-06-12 19:38:57 +0200

    Add versions manager command
    Closes osism/python-osism#462
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ osism/commands/versions.py
@@ -0,0 +1,35 @@
+from cliff.command import Command
+import docker
+import tabulate
+
+
+class Manager(Command):
+    def get_parser(self, prog_name):
+        parser = super(Manager, self).get_parser(prog_name)
+        return parser
+
+    def take_action(self, parsed_args):
+        client = docker.from_env()
+
+        data = []
+
+        for cname in ["osism-ansible", "ceph-ansible", "kolla-ansible"]:
+            try:
+                container = client.containers.get(cname)
+                version = container.labels["org.opencontainers.image.version"]
+
+                if cname == "ceph-ansible":
+                    mrelease = container.labels["de.osism.release.ceph"]
+                elif cname == "kolla-ansible":
+                    mrelease = container.labels["de.osism.release.openstack"]
+                else:
+                    mrelease = ""
+
+                data.append([cname, version, mrelease])
+            except docker.errors.NotFound:
+                pass
+
+        result = tabulate.tabulate(
+            data, headers=["Module", "OSISM version", "Module release"], tablefmt="psql"
+        )
+        print(result)
