88a97adea4700c0ad6dfde55363d7a136e76e1ef
Christian Berendt <berendt@osism.tech>
2023-06-16 10:40:40 +0200

    Add get tasks command
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/get.py
+++ osism/commands/get.py
@@ -1,6 +1,11 @@
+from datetime import datetime
+
+from celery import Celery
 from cliff.command import Command
-import docker
-import tabulate
+from redis import Redis
+from tabulate import tabulate
+
+from osism.tasks import Config
 
 
 class VersionsManager(Command):
@@ -29,7 +34,65 @@ class VersionsManager(Command):
             except docker.errors.NotFound:
                 pass
 
-        result = tabulate.tabulate(
+        result = tabulate(
             data, headers=["Module", "OSISM version", "Module release"], tablefmt="psql"
         )
         print(result)
+
+
+class Tasks(Command):
+    def get_parser(self, prog_name):
+        parser = super(Tasks, self).get_parser(prog_name)
+        parser.add_argument(
+            "--status", default="all", help="Status of the tasks to list"
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        status = parsed_args.status
+        redis = Redis(host="redis", port="6379")
+
+        app = Celery("task")
+        app.config_from_object(Config)
+
+        i = app.control.inspect()
+
+        table = []
+
+        task_status = "ACTIVE"
+        for worker, tasks in i.active().items():
+            for task in tasks:
+                time_start = datetime.fromtimestamp(task["time_start"])
+                table.append(
+                    [
+                        worker,
+                        task["id"],
+                        task["name"],
+                        task_status,
+                        time_start,
+                        task["args"],
+                    ]
+                )
+
+        task_status = "SCHEDULED"
+        for worker, tasks in i.scheduled().items():
+            for task in tasks:
+                time_start = datetime.fromtimestamp(task["time_start"])
+                table.append(
+                    [
+                        worker,
+                        task["id"],
+                        task["name"],
+                        task_status,
+                        time_start,
+                        task["args"],
+                    ]
+                )
+
+        print(
+            tabulate(
+                table,
+                headers=["Worker", "ID", "Name", "Status", "Start time", "Arguments"],
+                tablefmt="psql",
+            )
+        )
--- osism/commands/task.py
+++ osism/commands/task.py
@@ -1,69 +1,7 @@
-from datetime import datetime
-
 from celery import Celery
 from cliff.command import Command
-from redis import Redis
-from tabulate import tabulate
 
 from osism.tasks import Config
-
-
-class List(Command):
-    def get_parser(self, prog_name):
-        parser = super(List, self).get_parser(prog_name)
-        parser.add_argument(
-            "--status", default="all", help="Status of the tasks to list"
-        )
-        return parser
-
-    def take_action(self, parsed_args):
-        status = parsed_args.status
-        redis = Redis(host="redis", port="6379")
-
-        app = Celery("task")
-        app.config_from_object(Config)
-
-        i = app.control.inspect()
-
-        table = []
-
-        task_status = "ACTIVE"
-        for worker, tasks in i.active().items():
-            for task in tasks:
-                time_start = datetime.fromtimestamp(task["time_start"])
-                table.append(
-                    [
-                        worker,
-                        task["id"],
-                        task["name"],
-                        task_status,
-                        time_start,
-                        task["args"],
-                    ]
-                )
-
-        task_status = "SCHEDULED"
-        for worker, tasks in i.scheduled().items():
-            for task in tasks:
-                time_start = datetime.fromtimestamp(task["time_start"])
-                table.append(
-                    [
-                        worker,
-                        task["id"],
-                        task["name"],
-                        task_status,
-                        time_start,
-                        task["args"],
-                    ]
-                )
-
-        print(
-            tabulate(
-                table,
-                headers=["Worker", "ID", "Name", "Status", "Start time", "Arguments"],
-                tablefmt="psql",
-            )
-        )
 
 
 class Revoke(Command):
