710a40b4982feb6c478e17da4614822c63beb879
Christian Berendt <berendt@osism.tech>
2023-10-25 20:34:23 +0200

    Add --number-of-workers parameter to worker command (#630)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/worker.py
+++ osism/commands/worker.py
@@ -8,11 +8,19 @@ from cliff.command import Command
 class Run(Command):
     def get_parser(self, prog_name):
         parser = super(Run, self).get_parser(prog_name)
+        parser.add_argument(
+            "--number-of-workers",
+            "-n",
+            default=16,
+            type=int,
+            help="Number of workers",
+        )
         parser.add_argument("type", nargs=1, type=str, help="Type of the worker")
         return parser
 
     def take_action(self, parsed_args):
         queue = parsed_args.type[0]
+        number_of_workers = parsed_args.number_of_workers
 
         if queue in ["openstack", "netbox", "conductor"]:
             tasks = queue
@@ -22,12 +30,12 @@ class Run(Command):
         # NOTE: use python interface in the future, works for the moment
         if tasks == "osism":
             p = subprocess.Popen(
-                f"celery -A osism.tasks.ansible worker -n {queue} --loglevel=INFO -Q {queue}",
+                f"celery -A osism.tasks.ansible worker -n {queue} --loglevel=INFO -Q {queue} -c {number_of_workers}",
                 shell=True,
             )
         else:
             p = subprocess.Popen(
-                f"celery -A osism.tasks.{tasks} worker -n {queue} --loglevel=INFO -Q {queue}",
+                f"celery -A osism.tasks.{tasks} worker -n {queue} --loglevel=INFO -Q {queue} -c {number_of_workers}",
                 shell=True,
             )
         p.wait()
