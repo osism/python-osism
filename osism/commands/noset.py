d7cb69293e147131ad7ede715e854fa599e68cd4
Christian Berendt <berendt@osism.tech>
2023-10-27 18:37:04 +0200

    Fix & improve the set/noset maintenance command (#646)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/noset.py
+++ osism/commands/noset.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from cliff.command import Command
+from loguru import logger
 
 from osism.tasks import ansible
 
@@ -19,14 +20,16 @@ class NoMaintenance(Command):
     def take_action(self, parsed_args):
         host = parsed_args.host[0]
 
+        logger.info(f"Set no maintenance state on host {host}")
+
         arguments = [
             "-e status=False",
             "-l {host}",
         ]
 
         ansible.run.delay(
-            "state",
-            "maintenance",
+            "generic",
+            "state-maintenance",
             arguments,
             publish=False,
             locking=False,
