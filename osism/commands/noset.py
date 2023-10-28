0d06a8f546f3c249522511e9d81efd83d8cb9948
Christian Berendt <berendt@osism.tech>
2023-10-28 21:59:54 +0200

    Add set/noset bootstrap command (#648)
    Closes osism/issues#616
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/noset.py
+++ osism/commands/noset.py
@@ -32,3 +32,31 @@ class NoMaintenance(Command):
             "state-maintenance",
             arguments,
         )
+
+
+class NoBootstrap(Command):
+    def get_parser(self, prog_name):
+        parser = super(NoBootstrap, self).get_parser(prog_name)
+        parser.add_argument(
+            "host",
+            nargs=1,
+            type=str,
+            help="Host that should no longer be set to bootstrapped",
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        host = parsed_args.host[0]
+
+        logger.info(f"Set not bootstrapped state on host {host}")
+
+        arguments = [
+            "-e status=False",
+            f"-l {host}",
+        ]
+
+        ansible.run.delay(
+            "generic",
+            "state-bootstrap",
+            arguments,
+        )
--- osism/commands/set.py
+++ osism/commands/set.py
@@ -32,3 +32,31 @@ class Maintenance(Command):
             "state-maintenance",
             arguments,
         )
+
+
+class Bootstrap(Command):
+    def get_parser(self, prog_name):
+        parser = super(Bootstrap, self).get_parser(prog_name)
+        parser.add_argument(
+            "host",
+            nargs=1,
+            type=str,
+            help="Host to be set to bootstrapped",
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        host = parsed_args.host[0]
+
+        logger.info(f"Set bootstraped state on host {host}")
+
+        arguments = [
+            "-e status=True",
+            f"-l {host}",
+        ]
+
+        ansible.run.delay(
+            "generic",
+            "state-bootstrap",
+            arguments,
+        )
