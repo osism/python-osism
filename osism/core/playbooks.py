377c9542164d537595cea83849fa6bd06b2e1c9c
Christian Berendt <berendt@osism.tech>
2024-05-02 12:56:56 +0200

    Run fix-973 in the osism-ansible environment
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/core/playbooks.py
+++ osism/core/playbooks.py
@@ -1,14 +1,25 @@
 # SPDX-License-Identifier: Apache-2.0
 
+import os
 from pathlib import Path
 from typing import Dict, Any
 
+from loguru import logger
 import yaml
 
 MAP_ROLE2ENVIRONMENT: Dict[str, Any] = {}
+MAP_ROLE2RUNTIME: Dict[str, Any] = {}
 
 for path in Path("/interface/playbooks").glob("*-ansible.yml"):
-    with open(path) as fp:
-        data = yaml.load(fp, Loader=yaml.SafeLoader)
+    try:
+        with open(path) as fp:
+            data = yaml.load(fp, Loader=yaml.SafeLoader)
 
-    MAP_ROLE2ENVIRONMENT = MAP_ROLE2ENVIRONMENT | data
+        MAP_ROLE2ENVIRONMENT = MAP_ROLE2ENVIRONMENT | data
+        MAP_ROLE2RUNTIME[os.path.basename(path)[:-4]] = data.keys()
+
+    # Ignore YAML errors here so that we can prevent the command
+    # from being non-functional if a runtime environment provides
+    # an invalid interface file.
+    except yaml.YAMLError as e:
+        logger.warning(e)
