397481c8a73dd601f703aac48824175412c559b1
Christian Berendt <berendt@osism.tech>
2024-09-28 20:55:22 +0200

    Add manage compute stop command
    ```
    $ osism manage compute stop testbed-node-3
    Stop server 388eccb2-e6ce-4631-a621-4edc4402fac6 (test) [yes/no]: yes
    2024-09-28 18:58:39 | INFO     | Stopping server 388eccb2-e6ce-4631-a621-4edc4402fac6
    ```
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/compute.py
+++ osism/commands/compute.py
@@ -2,8 +2,10 @@
 
 from cliff.command import Command
 import keystoneauth1
+from loguru import logger
 import openstack
 from tabulate import tabulate
+from prompt_toolkit import prompt
 
 
 def get_cloud_connection():
@@ -41,3 +43,44 @@ class ComputeList(Command):
                 tablefmt="psql",
             )
         )
+
+
+class ComputeStop(Command):
+    def get_parser(self, prog_name):
+        parser = super(ComputeStop, self).get_parser(prog_name)
+        parser.add_argument(
+            "--yes",
+            default=False,
+            help="Always say yes",
+            action="store_true",
+        )
+        parser.add_argument(
+            "host",
+            nargs=1,
+            type=str,
+            help="Host on that all running instances are to be stopped",
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        yes = parsed_args.yes
+        host = parsed_args.host[0]
+        conn = get_cloud_connection()
+        result = []
+
+        for server in conn.compute.servers(all_projects=True, node=host):
+            result.append([server.id, server.name, server.status])
+
+        for server in result:
+            if server[2] not in ["ACTIVE"]:
+                logger.info(
+                    f"{server[0]} ({server[1]}) in status {server[2]} cannot be stopped"
+                )
+                continue
+
+            if not yes:
+                answer = prompt(f"Stop server {server[0]} ({server[1]}) [yes/no]: ")
+
+            if answer in ["yes", "y"]:
+                logger.info(f"Stopping server {server[0]}")
+                conn.compute.stop_server(server[0])
