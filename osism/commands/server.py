852159ae1902a8c312880219d9c1235f83ec3868
Christian Berendt <berendt@osism.tech>
2024-10-07 21:38:42 +0200

    Add osism manage server migrate command (#1083)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/server.py
+++ osism/commands/server.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from datetime import datetime, timezone
+import time
 
 from cliff.command import Command
 import dateutil
@@ -8,8 +9,7 @@ import keystoneauth1
 from loguru import logger
 import openstack
 from tabulate import tabulate
-
-# from prompt_toolkit import prompt
+from prompt_toolkit import prompt
 
 
 def get_cloud_connection():
@@ -19,6 +19,84 @@ def get_cloud_connection():
         pass
 
     return conn
+
+
+class ServerMigrate(Command):
+    def get_parser(self, prog_name):
+        parser = super(ServerMigrate, self).get_parser(prog_name)
+        parser.add_argument(
+            "--yes",
+            default=False,
+            help="Always say yes",
+            action="store_true",
+        )
+        parser.add_argument(
+            "--no-wait",
+            default=False,
+            help="Do not wait for completion of migration",
+            action="store_true",
+        )
+        parser.add_argument(
+            "--force",
+            default=False,
+            help="Force a live-migration by not verifying the provided destination host by the scheduler. ",
+            action="store_true",
+        )
+        parser.add_argument(
+            "--target",
+            default=None,
+            type=str,
+            help="Host to which the rinstance is to be migrated",
+        )
+        parser.add_argument(
+            "instance",
+            nargs=1,
+            type=str,
+            help="Instance to be migrated (specify UUID)",
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        yes = parsed_args.yes
+        instance = parsed_args.instance[0]
+        target = parsed_args.target
+        force = parsed_args.force
+        no_wait = parsed_args.no_wait
+
+        conn = get_cloud_connection()
+
+        result = conn.compute.get_server(instance)
+        server = [result.id, result.name, result.status]
+
+        if server[2] not in ["ACTIVE", "PAUSED"]:
+            logger.info(
+                f"{server[0]} ({server[1]}) in status {server[2]} cannot be live migrated"
+            )
+            return
+
+        if yes:
+            answer = "yes"
+        else:
+            answer = prompt(f"Live migrate server {server[0]} ({server[1]}) [yes/no]: ")
+
+        if answer in ["yes", "y"]:
+            logger.info(f"Live migrating server {server[0]}")
+            conn.compute.live_migrate_server(
+                server[0], host=target, block_migration="auto", force=force
+            )
+
+            if not no_wait:
+                inner_wait = True
+                while inner_wait:
+                    time.sleep(2)
+                    s = conn.compute.get_server(server[0])
+                    if s.status in ["MIGRATING"]:
+                        logger.info(
+                            f"Live migration of {server[0]} ({server[1]}) is still in progress"
+                        )
+                        inner_wait = True
+                    else:
+                        inner_wait = False
 
 
 class ServerList(Command):
