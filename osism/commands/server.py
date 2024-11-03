70ea6c6b7a5883b05e66107e0461f32341683d18
Christian Berendt <berendt@osism.tech>
2024-11-03 18:50:29 +0100

    Remove duplicate get_cloud_connection methods (#1114)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/commands/server.py
+++ osism/commands/server.py
@@ -5,20 +5,11 @@ import time
 
 from cliff.command import Command
 import dateutil
-import keystoneauth1
 from loguru import logger
-import openstack
 from tabulate import tabulate
 from prompt_toolkit import prompt
 
-
-def get_cloud_connection():
-    try:
-        conn = openstack.connect(cloud="admin")
-    except keystoneauth1.exceptions.auth_plugins.MissingRequiredOptions:
-        pass
-
-    return conn
+from osism.commands import get_cloud_connection
 
 
 class ServerMigrate(Command):
