73a2ae09ad9fe598acc6abe14d565931c6a0df0e
janhorstmann <horstmann@osism.tech>
2025-03-14 18:54:02 +0100

    Replace netbox subcommands with netbox-manager (#1214)
    Part of https://github.com/osism/issues/issues/1223
    
    Signed-off-by: Jan Horstmann <horstmann@osism.tech>

--- osism/settings.py
+++ osism/settings.py
@@ -27,9 +27,6 @@ NETBOX_URL = os.getenv("NETBOX_API")
 NETBOX_TOKEN = os.getenv("NETBOX_TOKEN", read_secret("NETBOX_TOKEN"))
 IGNORE_SSL_ERRORS = os.getenv("IGNORE_SSL_ERRORS", "True") == "True"
 
-BASE_PATH = os.getenv("BASE_PATH", "/devicetype-library/device-types/")
-VENDORS = os.getenv("VENDORS", "").split()
-
 # 43200 seconds = 12 hours
 GATHER_FACTS_SCHEDULE = float(os.getenv("GATHER_FACTS_SCHEDULE", "43200.0"))
 INVENTORY_RECONCILER_SCHEDULE = float(
--- osism/tasks/__init__.py
+++ osism/tasks/__init__.py
@@ -160,20 +160,6 @@ def run_ansible_in_environment(
             env=env,
         )
 
-    # execute local netbox playbooks
-    elif worker == "osism-ansible" and environment == "netbox-local":
-        if locking:
-            lock.acquire()
-
-        command = f"/run-{environment}.sh {role} {joined_arguments}"
-        logger.info(f"RUN {command}")
-        p = subprocess.Popen(
-            command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            shell=True,
-        )
-
     # execute all other roles
     else:
         if locking:
@@ -189,6 +175,50 @@ def run_ansible_in_environment(
             env=env,
         )
 
+    while p.poll() is None:
+        line = p.stdout.readline().decode("utf-8")
+        if publish:
+            redis.xadd(request_id, {"type": "stdout", "content": line})
+        result += line
+
+    rc = p.wait(timeout=60)
+
+    if publish:
+        redis.xadd(request_id, {"type": "rc", "content": rc})
+        redis.xadd(request_id, {"type": "action", "content": "quit"})
+
+    if locking:
+        lock.release()
+
+    return result
+
+
+def run_command(
+    request_id,
+    command,
+    env,
+    *arguments,
+    publish=True,
+    locking=False,
+    auto_release_time=3600,
+):
+    result = ""
+    command_env = os.environ.copy()
+    command_env.update(env)
+
+    if locking:
+        lock = Redlock(
+            key=f"lock-{command}",
+            masters={redis},
+            auto_release_time=auto_release_time,
+        )
+
+    p = subprocess.Popen(
+        [command] + list(arguments),
+        env=command_env,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
     while p.poll() is None:
         line = p.stdout.readline().decode("utf-8")
         if publish:
