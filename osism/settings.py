299e69b1b179b52fa4b74e0bafab48ad7f42986a
Christian Berendt <berendt@osism.tech>
2025-05-23 21:33:14 +0200

    Rename NETBOX_FILTER_LIST to NETBOX_FILTER_CONDUCTOR
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/settings.py
+++ osism/settings.py
@@ -35,8 +35,8 @@ INVENTORY_RECONCILER_SCHEDULE = float(
 
 OSISM_API_URL = os.getenv("OSISM_API_URL", None)
 
-NETBOX_FILTER_LIST = os.getenv(
-    "NETBOX_FILTER_LIST",
+NETBOX_FILTER_CONDUCTOR = os.getenv(
+    "NETBOX_FILTER_CONDUCTOR",
     "[{'state': 'active', 'tag': ['managed-by-ironic']}]",
 )
 
