07871b96baee114d498520271990612b59ebca52
Christian Berendt <berendt@osism.tech>
2025-05-23 21:48:49 +0200

    Rename NETBOX_FILTER_LIST to NETBOX_FILTER_CONDUCTOR (#1368)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- osism/tasks/conductor.py
+++ osism/tasks/conductor.py
@@ -33,7 +33,7 @@ def get_nb_device_query_list():
             "tag",
             "state",
         ]
-        nb_device_query_list = yaml.safe_load(settings.NETBOX_FILTER_LIST)
+        nb_device_query_list = yaml.safe_load(settings.NETBOX_FILTER_CONDUCTOR)
         if type(nb_device_query_list) is not list:
             raise TypeError
         for nb_device_query in nb_device_query_list:
@@ -55,11 +55,11 @@ def get_nb_device_query_list():
                         raise ValueError(f"Invalid name {value_name} for {key}")
     except (yaml.YAMLError, TypeError):
         logger.error(
-            f"Setting NETBOX_FILTER_LIST needs to be an array of mappings containing supported netbox device filters: {supported_nb_device_filters}"
+            f"Setting NETBOX_FILTER_CONDUCTOR needs to be an array of mappings containing supported netbox device filters: {supported_nb_device_filters}"
         )
         nb_device_query_list = []
     except ValueError as exc:
-        logger.error(f"Unknown value in NETBOX_FILTER_LIST: {exc}")
+        logger.error(f"Unknown value in NETBOX_FILTER_CONDUCTOR: {exc}")
         nb_device_query_list = []
 
     return nb_device_query_list
