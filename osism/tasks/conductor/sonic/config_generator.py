f4f229014fd416fad094600492eda8d2a9898c1c
Dr. Jens Harbott <harbott@osism.tech>
2026-02-13 15:00:30 +0100

    sonic: Add static default route in mgmt VRF
    Signed-off-by: Dr. Jens Harbott <harbott@osism.tech>

--- osism/tasks/conductor/sonic/config_generator.py
+++ osism/tasks/conductor/sonic/config_generator.py
@@ -255,6 +255,9 @@ def generate_sonic_config(device, hwsku, device_as_mapping=None, config_version=
         oob_ip, prefix_len = oob_ip_result
         config["MGMT_INTERFACE"]["eth0"] = {"admin_status": "up"}
         config["MGMT_INTERFACE"][f"eth0|{oob_ip}/{prefix_len}"] = {}
+        metalbox_ip = _get_metalbox_ip_for_device(device)
+        config["STATIC_ROUTE"] = {}
+        config["STATIC_ROUTE"]["mgmt|0.0.0.0/0"] = {"nexthop": metalbox_ip}
 
     # Add breakout configuration
     if breakout_info["breakout_cfgs"]:
